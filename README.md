b5343f6639e4807458a5acf31327939dc12e07f0
Aiden Lin <98539623+AidenL173@users.noreply.github.com>
2022-02-20 22:19:36 +0000

    Completion.

--- README.md
+++ README.md
@@ -8,18 +8,18 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ < 2 races between turtles using random values. Part B makes the turtle draw multiple different polygons. >
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: < 0 >
 
-Grace days remaining: < # >/5
+Grace days remaining: < 5 >/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+ < N/A >
 
 ## REFERENCES:
- < List any outside resources used >
+ < N/A >
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+ < N/A >
--- main.py
+++ main.py
@@ -1,26 +1,81 @@
-import turtle               #1. import modules
+import turtle
 import random
 
 #Part A
-window = turtle.Screen()       # 2.  Create a screen
+window = turtle.Screen()
 window.bgcolor('lightblue')
 
-michelangelo = turtle.Turtle()    # 3.  Create two turtles
+michelangelo = turtle.Turtle()
 leonardo = turtle.Turtle()
 michelangelo.color('orange')
 leonardo.color('blue')
 michelangelo.shape('turtle')
 leonardo.shape('turtle')
 
-michelangelo.up()          # 4.  Pick up the pen so we don’t get lines
+michelangelo.up()
 leonardo.up()
 michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
-## 5. your code goes here
+## 5. 
+michelangelo.speed(1)
+leonardo.speed(1)
+distance = random.randrange(1,101)
+michelangelo.forward(distance)
+distance = random.randrange(1,101)
+leonardo.forward(distance)
 
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 
-# Part B - complete part B here
+for i in range(1,11):
+  distance1 = random.randrange(0,11)
+  distance2 = random.randrange(0,11)
+  michelangelo.forward(distance1)
+  leonardo.forward(distance2)
 
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+
+#Part B
+michelangelo.goto(-25,50)
+michelangelo.down()
+for i in range(1,4):
+  michelangelo.forward(100)
+  michelangelo.right(360/3)
+michelangelo.clear()
+michelangelo.up()
+
+michelangelo.goto(-25,50)
+michelangelo.down()
+for i in range(1,5):
+  michelangelo.forward(100)
+  michelangelo.right(360/4)
+michelangelo.clear()
+michelangelo.up()
+
+michelangelo.goto(-25,50)
+michelangelo.down()
+for i in range(1,7):
+  michelangelo.forward(75)
+  michelangelo.right(360/6)
+michelangelo.clear()
+michelangelo.up()
+
+michelangelo.goto(-25,50)
+michelangelo.down()
+for i in range(1,10):
+  michelangelo.forward(50)
+  michelangelo.right(360/9)
+michelangelo.clear()
+michelangelo.up()
+
+michelangelo.goto(-25,50)
+michelangelo.down()
+for i in range(1,13):
+  michelangelo.forward(35)
+  michelangelo.right(360/12)
+michelangelo.clear()
+michelangelo.up()
 
 window.exitonclick()
